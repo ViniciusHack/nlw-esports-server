8796c674c834ef1886e300e5bf26094b474962ab
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-09-17 18:05:04 -0300

    feat: list games

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 
 const app = express();
+const prisma = new PrismaClient()
 
-app.post("/ads", (req, res) => {
-  return res.status(201).json({})
+app.get("/games", async (req, res) => {
+  const games = await prisma.game.findMany();
+
+  return res.json({ games });
 })
 
 app.get("/games/:id/ads", (req, res) => {
   // const gameId = req.params.id
   return res.json({})
 });
+
+app.post("/ads", (req, res) => {
+  return res.status(201).json({})
+})
+
 
 app.get("/ads/:id/discord", (req, res) => {
   // const adId = req.params.id
