bbf45009fbd6cc0dcfd24c4ce6d8f433f973b2e8
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-09-17 17:49:50 -0300

    feat: routes and database schema

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,18 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", () => {
-  
+app.post("/ads", (req, res) => {
+  return res.status(201).json({})
+})
+
+app.get("/games/:id/ads", (req, res) => {
+  // const gameId = req.params.id
+  return res.json({})
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+  // const adId = req.params.id
+  return res.json({})
 });
 
 app.listen(8888);
